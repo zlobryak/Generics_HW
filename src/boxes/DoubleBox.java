d7e0642805ab5a19f1382a92fcaded7dcb78632c
Zlobryak <clychnik@gmail.com>
2025-07-18 22:33:01 +0300

    Package for Boxes Using ArrayList instead of Arrow

--- src/boxes/DoubleBox.java
+++ src/boxes/DoubleBox.java
@@ -1,23 +1,26 @@
 package boxes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoubleBox<T> {
     private RandomBox<T> first;
     private RandomBox<T> second;
-    private int boxSize = 0; //Пробую сделпть то же самое через массив
-    RandomBox<T>[] boxArray = new RandomBox[2];
+    private int boxSize = 0;
+    List<RandomBox<T>> boxList = new ArrayList<>();
 
     public boolean put(int key, T value) {
         //проверить, пустая ли первая коробка.
-        if (first == null) {
+        if (first == null && boxList.isEmpty()) {
             //Положить объект в коробку, если она пустая
             first = new RandomBox<>(key, value);
-            boxArray[0] = first; //Дублирую это в массив, можно было обойтись без переменных
+            boxList.add(first); //Дублирую это в массив, можно было обойтись без переменных
             boxSize += 1;
             return true;
-        } else if (second == null) { //Иначе проверить пустая ли вторая коробка.
+        } else if (second == null && boxList.contains(first)) { //Иначе проверить пустая ли вторая коробка.
             //Если вторая пустая - положить объект во вторую коробку
             second = new RandomBox<>(key, value);
-            boxArray[1] = second; //Дублирую это в массив, можно было обойтись без переменных
+            boxList.add(second); //Дублирую это в массив, можно было обойтись без переменных
             boxSize += 1;
             return true;
         }
@@ -27,12 +30,11 @@ public class DoubleBox<T> {
     public T get(int key) {
         if (first != null && first.getKey() == key) {
 //            return first.tryUnlock(key);
-            return boxArray[0].tryUnlock(key); //То же жействие, но через массим
+            return boxList.get(0).tryUnlock(key); //То же действие, но через массив
         }
         if (second != null && second.getKey() == key) {
 //            return second.tryUnlock(key);
-            return boxArray[1].tryUnlock(key); //Тоже действие, но через массив
-
+            return boxList.get(1).tryUnlock(key); //То же действие, но через массив
         }
         return null;
     }
--- src/boxes/RandomBox.java
+++ src/boxes/RandomBox.java
@@ -3,8 +3,8 @@ package boxes;
 //Перед вами готовый дженерик-класс RandomBox.
 //В неё можно положить любой объект и ключ к нему.
 public class RandomBox<T> {
-    private int key;
-    private T value;
+    private final int key;
+    private final T value;
 
     public RandomBox(int key, T value) {
         this.key = key;
